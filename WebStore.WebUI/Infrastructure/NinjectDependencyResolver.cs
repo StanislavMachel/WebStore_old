69cfe2cff90b4bc6d76f6f9fa616644054a91e01
Stanislav Matšel <stanislav.matsel@ee.abb.com>
2016-03-25 13:31:36 +0200

    Add NinjectDependencyResolver resolver to WebUI

---
+++ WebStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Ninject;
+
+namespace WebStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kelner;
+        public NinjectDependencyResolver(IKernel kelnerParam)
+        {
+            kelner = kelnerParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kelner.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kelner.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            //put buindings here
+        }
+    }
+}
