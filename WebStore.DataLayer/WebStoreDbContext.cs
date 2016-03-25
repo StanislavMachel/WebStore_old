bf87583044cc03f5e303d8122af4e30e4671d3ac
Stanislav Matšel <stanislav.matsel@ee.abb.com>
2016-03-25 13:44:02 +0200

    Add DataLayer

---
+++ WebStore.DataLayer/WebStoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WebStore.Domain.Entities;
+
+namespace WebStore.DataLayer
+{
+    public class WebStoreDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
