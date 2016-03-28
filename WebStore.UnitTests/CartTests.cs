8094f4f79811c6b212114835f3ef7da16bcdceae
Stanislav Matšel <stanislav.matsel@ee.abb.com>
2016-03-28 22:53:55 +0300

    Implement tests form Cart class

---
+++ WebStore.UnitTests/CartTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void car_add_new_products()
+        {
+            Cart testCart = new Cart();
+
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            testCart.AddProduct(p1, 1);
+            testCart.AddProduct(p2, 1);
+
+            IEnumerable<ProductQuantity> result = testCart.ProductQuantityCollection;
+
+
+            Assert.AreEqual(result.Count(), 2);
+            Assert.AreEqual(result.ElementAt(0).Product, p1);
+            Assert.AreEqual(result.ElementAt(1).Product, p2);
+        }
+
+        [TestMethod]
+        public void can_add_quantity_for_existing_product_quantity()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            Cart testCart = new Cart();
+
+            testCart.AddProduct(p1, 1);
+            testCart.AddProduct(p2, 1);
+            testCart.AddProduct(p2, 10);
+
+            IEnumerable<ProductQuantity> results = testCart.ProductQuantityCollection;
+
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results.ElementAt(0).Product, p1);
+            Assert.AreEqual(results.ElementAt(1).Product, p2);
+            Assert.AreEqual(results.ElementAt(0).Quantity, 1);
+            Assert.AreEqual(results.ElementAt(1).Quantity, 11);
+
+        }
+
+        [TestMethod]
+        public void can_remove_product()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+
+            Cart testCart = new Cart();
+
+            testCart.AddProduct(p1, 1);
+            testCart.AddProduct(p2, 2);
+            testCart.AddProduct(p3, 3);
+            testCart.AddProduct(p2, 2);
+
+            testCart.RemoveProduct(p2);
+
+            IEnumerable<ProductQuantity> results = testCart.ProductQuantityCollection;
+
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results.Where(p => p.Product == p2).Count(), 0);
+
+        }
+        [TestMethod]
+        public void calculate_total_cost()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 450M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            Cart testCart = new Cart();
+
+            testCart.AddProduct(p1, 1);
+            testCart.AddProduct(p2, 2);
+            testCart.AddProduct(p2, 2);
+
+            decimal result = testCart.ComputeTotalCost();
+
+            Assert.AreEqual(result, 650M);
+
+        }
+        [TestMethod]
+        public void can_clear_cart()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 450M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            Cart testCart = new Cart();
+
+            testCart.AddProduct(p1, 1);
+            testCart.AddProduct(p2, 2);
+            testCart.AddProduct(p2, 2);
+
+            testCart.Clear();
+
+            Assert.AreEqual(testCart.ProductQuantityCollection.Count(), 0);
+
+        }
+    }
+}
---
+++ WebStore.UnitTests/NavigationControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Domain.Abstract;
+using WebStore.Domain.Entities;
+using System.Linq;
+using System.Collections.Generic;
+using WebStore.WebUI.Controllers;
+using System.Web.Mvc;
+namespace WebStore.UnitTests
+{
+    [TestClass]
+    public class NavigationControllerTests
+    {
+        [TestMethod]
+        public void can_create_categories()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.GetProducts())
+                .Returns((new List<Product>()
+                    {
+                        new Product {ProductID = 1, Name = "P1", Category = "C1" },
+                        new Product {ProductID = 1, Name = "P2", Category = "C1" },
+                        new Product {ProductID = 1, Name = "P3", Category = "C2" },
+                        new Product {ProductID = 1, Name = "P4", Category = "C3" }
+                    }).AsQueryable());
+
+            NavigationController testController = new NavigationController(mock.Object);
+            IEnumerable<string> result = (IEnumerable<string>)testController.Menu().Model;
+
+            Assert.AreEqual(result.Count(), 3);
+            Assert.AreEqual(result.ElementAt(0), "C1");
+            Assert.AreEqual(result.ElementAt(1), "C2");
+            Assert.AreEqual(result.ElementAt(2), "C3");
+        }
+    }
+}
