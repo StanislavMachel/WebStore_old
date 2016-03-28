ce94ab55f22578d02f06d9b8a2be37831c552f6a
Stanislav Matšel <stanislav.matsel@ee.abb.com>
2016-03-28 22:11:26 +0300

    Implement Cart and ProductQuantity classes

--- WebStore.Domain/Entities/Cart.cs
+++ WebStore.Domain/Entities/Cart.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace WebStore.Domain.Entities
 {
-    class Cart
+    public class Cart
     {
+        private List<ProductQuantity> productQuantityCollection = new List<ProductQuantity>();
+
+        public void AddProduct(Product product, int quantity)
+        {
+            ProductQuantity productQuantity = productQuantityCollection
+                                            .Where(p => p.Product.ProductID == product.ProductID)
+                                            .FirstOrDefault();
+
+            if(productQuantity == null)
+            {
+                productQuantityCollection.Add(new ProductQuantity
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                productQuantity.Quantity += quantity;
+            }
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            productQuantityCollection.RemoveAll(p => p.Product.ProductID == product.ProductID);
+        }
+
+        public decimal ComputeTotalCost()
+        {
+            return productQuantityCollection.Sum(pq => pq.Product.Price * pq.Quantity);
+        }
+
+        public void Clear()
+        {
+            productQuantityCollection.Clear();
+        }
+
+        public IEnumerable<ProductQuantity> ProductQuantityCollection
+        {
+            get
+            {
+                return productQuantityCollection;
+            }
+        }
+
     }
 }
---
+++ WebStore.Domain/Entities/ProductQuantity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Domain.Entities
+{
+    public class ProductQuantity
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
